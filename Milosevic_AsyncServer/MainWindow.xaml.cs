8778092bf06cd1ac4f0947ae68a0e29fcff531c1
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-01-22 12:40:53 +0100

    Add project files.

---
+++ Milosevic_AsyncServer/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using AsyncSocketLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Milosevic_AsyncServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AsyncSocketServer mServer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mServer = new AsyncSocketServer();
+        }
+
+        private void btn_ascolto_Click(object sender, RoutedEventArgs e)
+        {
+            mServer.InAscolto();
+        }
+
+        private void btn_disconnettiti_Click(object sender, RoutedEventArgs e)
+        {
+            mServer.Disconnetti();
+        }
+    }
+}
